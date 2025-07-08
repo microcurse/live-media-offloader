8cce28cfeb7c6328481135f8ed527275c6dc712e
Marc <marc.maninang@gmail.com>
2025-07-08 14:18:33 -0700

    Update version to 1.0.4 and refactor media URL handling with output buffering for improved performance.

--- live-media-offloader.php
+++ live-media-offloader.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Live Media Offloader
  * Plugin URI:        https://github.com/microcurse/live-media-offloader
  * Description:       Reference the media library from the live environment.
- * Version:           1.0.0
+ * Version:           1.0.4
  * Author:            Marc Maninang
  * Author URI:        https://github.com/microcurse/
  * License:           GPL v2 or later
@@ -12,19 +12,46 @@
  */
 
 if ( ! defined( 'WPINC' ) ) {
-    die;
+	die;
 }
 
-function lmo_filter_media_url( $url, $post_id ) {
-    if ( defined( 'LMO_LIVE_SITE_URL' ) && LMO_LIVE_SITE_URL ) {
-        $live_url = LMO_LIVE_SITE_URL;
-        $local_url = get_site_url();
+/**
+ * Replaces the local site URL with the live site URL in the provided buffer.
+ *
+ * @param string $buffer The output buffer.
+ * @return string The modified buffer.
+ */
+function lmo_url_replace_callback( $buffer ) {
+	$local_url = get_site_url();
+	$live_url  = LMO_LIVE_SITE_URL;
 
-        // Ensure we don't replace the URL on the live site itself
-        if ( $local_url !== $live_url ) {
-            return str_replace( $local_url, $live_url, $url );
-        }
-    }
-    return $url;
+	$local_uploads_url = $local_url . '/wp-content/uploads';
+	$live_uploads_url  = $live_url . '/wp-content/uploads';
+
+	return str_replace( $local_uploads_url, $live_uploads_url, $buffer );
 }
-add_filter( 'wp_get_attachment_url', 'lmo_filter_media_url', 10, 2 );
+
+/**
+ * Starts output buffering to replace media URLs.
+ */
+function lmo_start_buffering() {
+	// Ensure the constant is defined and we are not on the live site.
+	if ( defined( 'LMO_LIVE_SITE_URL' ) && LMO_LIVE_SITE_URL ) {
+		$local_url = get_site_url();
+		$live_url  = LMO_LIVE_SITE_URL;
+		if ( $local_url !== $live_url ) {
+			ob_start( 'lmo_url_replace_callback' );
+		}
+	}
+}
+add_action( 'init', 'lmo_start_buffering' );
+
+/**
+ * Flushes the output buffer on shutdown.
+ */
+function lmo_flush_buffer() {
+	if ( ob_get_level() > 0 ) {
+		ob_end_flush();
+	}
+}
+add_action( 'shutdown', 'lmo_flush_buffer' );
